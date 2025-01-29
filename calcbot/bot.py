da2750ec12302aeedc0b7431fb5bab14541cf968
DaniilIslamgaliev <dante9226@mail.ru>
2025-01-29 15:20:06 +0300

    Add callback

--- calcbot/bot.py
+++ calcbot/bot.py
@@ -3,6 +3,9 @@ import telebot
 DLineCalc_bot = telebot.TeleBot('7776091392:AAE4_c9LL0kEeAadkNIpkMVNeopQ4yY4mZs')
 
 
+num = ''
+null = ''
+answer = ''
 first_msg = None
 keyboard = types.InlineKeyboardMarkup(row_width=4)
 button_eraser = types.InlineKeyboardButton(text='⬅️', callback_data='erase')
@@ -49,4 +52,80 @@ def calc(message):
         DLineCalc_bot.send_message(message.from_user.id, "0", reply_markup=keyboard)
 
 
+@DLineCalc_bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    global null, num
+    if call.message:
+        if call.data == 'none':
+            DLineCalc_bot.answer_callback_query(call.id)
+        if call.data == 'cancel':
+            num = ''
+            DLineCalc_bot.answer_callback_query(call.id)
+        if call.data == 'comma':
+            num += '.'
+            DLineCalc_bot.answer_callback_query(call.id)
+        if call.data == 'num_1':
+            num += '1'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_2':
+            num += '2'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_3':
+            num += '3'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_4':
+            num += '4'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_5':
+            num += '5'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_6':
+            num += '6'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_7':
+            num += '7'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_8':
+            num += '8'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_9':
+            num += '9'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'num_0':
+            num += '0'
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'plus':
+            num += str("+")
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'minus':
+            num += str("-")
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'multiply':
+            num += str("*")
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'division':
+            num += str("/")
+            DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'equals':
+            if num == '':
+                num = '0'
+                DLineCalc_bot.answer_callback_query(call.id)
+            else:
+                num = eval(num)
+                DLineCalc_bot.answer_callback_query(call.id)
+        elif call.data == 'erase':
+            if num != '':
+                num = num[:len(num)-1]
+                DLineCalc_bot.answer_callback_query(call.id)
+        if num != null:
+            if num == '':
+                DLineCalc_bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='0',
+                                                reply_markup=keyboard)
+            else:
+                DLineCalc_bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text=num,
+                                                reply_markup=keyboard)
+            DLineCalc_bot.answer_callback_query(call.id)
+            null = num
 
+
+DLineCalc_bot.infinity_polling()
